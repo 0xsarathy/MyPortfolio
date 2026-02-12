0696804e4afaef2d5bf0c81ee7a891621a22fe22
0xsarathy <chiyaansarathy2004@gmail.com>
2026-02-12 16:40:06 +0530

    completed

---
+++ script.js
@@ -0,0 +1,5 @@
+function showCerts() {
+  const certSection = document.getElementById("certifications");
+  certSection.classList.toggle("hidden");
+  certSection.scrollIntoView({ behavior: "smooth" });
+}
